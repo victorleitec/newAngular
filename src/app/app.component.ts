40d1558affca78c3f3a82edfcb3da9f8c76a7a5d
Victor Leite Costa <victorleitecosta@outlook.com.br>
2021-08-05 16:03:39 -0300

    Angular 10 Front-END

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import {Component} from '@angular/core';
+import {FormControl, FormGroup} from "@angular/forms";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+  form: FormGroup = new FormGroup({
+    description: new FormControl('')
+  })
+
+  submit() {
+    console.log(this.form.value)
+  }
+}
