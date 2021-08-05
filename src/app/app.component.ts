661ba3428bbebec84ddeadab5b17f173090a18ab
Victor Leite Costa <victorleitecosta@outlook.com.br>
2021-08-05 17:34:44 -0300

    Angular 10 Final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import {Component} from '@angular/core';
-import {FormControl, FormGroup} from "@angular/forms";
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {TodoService} from './todo.service';
+import {Todo} from './todo';
 
 @Component({
   selector: 'app-root',
@@ -7,12 +9,49 @@ import {FormControl, FormGroup} from "@angular/forms";
   styleUrls: ['./app.component.css']
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  todos: Todo[] = []
   form: FormGroup = new FormGroup({
-    description: new FormControl('')
+    description: new FormControl('', [Validators.required, Validators.minLength(3)])
   })
 
+  constructor(private service: TodoService) {
+  }
+
+  ngOnInit() {
+    this.listarTodos()
+  }
+
+  listarTodos() {
+    this.service.listar().subscribe(todoList => {
+      this.todos = todoList
+    })
+  }
+
+
   submit() {
-    console.log(this.form.value)
+    const todo: Todo = {...this.form.value};
+    this.service
+      .salvar(todo)
+      .subscribe(savedTodo => {
+        this.todos.push(savedTodo)
+        this.form.reset()
+      })
+  }
+
+  delete(todo: Todo) {
+    this.service.deletar(todo.id)
+      .subscribe({next: (response) => this.listarTodos()})
+  }
+
+  done(todo: Todo) {
+    this.service.marcarConcluido(todo.id)
+      .subscribe({
+        next: (todoAtualizado) => {
+          todo.done = todoAtualizado.done
+          todo.doneDate = todoAtualizado.doneDate
+        }
+      })
   }
 }
